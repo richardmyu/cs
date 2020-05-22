baf3a702dff6b1f97e7b75a2f7d2c54e6dd29a56
richyu <richardminyu@foxmail.com>
2020-05-22 22:31:09 +0800

    增加python第一二周课程笔记

---
+++ 程序设计入门-python语言/WEEK02/demo/dataType.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# <class 'str'>
+print(type('hello'))
+
+# <class 'int'>
+print(type(12))
+print(type(12))
+print(type(0x12))
+
+# <class 'float'>
+print(type(12.11))
+
+# <class 'bool'>
+print(type(True))
+print(type(False))
+
+# <class 'complex'>
+print(type(1 + 2j))
