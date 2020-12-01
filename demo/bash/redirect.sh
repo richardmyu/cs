c0ebb02cd95ffd54cd2b18028ea612bb804bf774
richyu <richardminyu@foxmail.com>
2020-12-01 23:43:29 +0800

    add bash note:redirect

---
+++ demo/bash/redirect.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# who > testRed
+
+# cat testRed
+
+# echo "何时问明月" > testRed
+
+# cat testRed
+
+# echo "会当凌绝顶" >> testRed
+
+# cat testRed
+
+# wc -l testRed
+
+# wc -l < testRed
+
+cat << EOF
+嘎嘎
+瓜瓜
+略略略
+EOF
+
+read -p 'wait' t
+
