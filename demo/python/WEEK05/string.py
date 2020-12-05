2b47ce1568e6daaf1694d9ed60f47ee53f4903d0
yum <richardminyu@foxmail.com>
2020-12-06 01:13:07 +0800

    doce(python): add 字符串

---
+++ demo/python/WEEK05/string.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+strSingle = 'hi\' hello\nworld!\nhhh'
+
+strQuote = "hi\' hello\nworld!\nhhh"
+
+strTriple = """hi\'
+ hello\n
+
+ world!\nhhh
+ """
+
+print("-----------------------")
+print(strSingle)
+print("-----------------------")
+print(strQuote)
+print("-----------------------")
+print(strTriple)
+print("-----------------------")
