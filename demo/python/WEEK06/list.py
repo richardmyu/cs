6f243db6e3771e49494d0aa2051ca3331743ee7f
yum <richardminyu@foxmail.com>
2020-12-07 22:01:18 +0800

    doce(python): add WEEK06

---
+++ demo/python/WEEK06/list.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+lst = [5, "he", "she", 7]
+
+# []
+print(lst[0])
+
+# [:]
+print(lst[1:3])
+
+# +
+print(lst+["it"])
+
+# *
+print(lst[1]*3)
+
+# in
+print('he' in lst)
+
+# len
+print(len(lst))
+
+# for
+for l in lst:
+    print(l)
+
+# append
+lst.append('hello')
+
+# extend
+lst.extend(['world'])
+
+# insert
+# print(lst.insert(1,3))
+
+# pop
+
+# remove
+
+# sort
+
+# reverse
+
+print(lst)
---
+++ demo/python/WEEK06/tuple.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+tup = (1, 'it', 'is')
+
+print(tup[0])
+
+print(tup[1:])
