2aab12e64d26435dcde2fb75feffc01ad8b81c5e
yum <richardminyu@foxmail.com>
2021-12-30 23:51:47 +0800

    add sql 06

---
+++ demo/sql/answer/ch_06/6_1.sql
@@ -0,0 +1,20 @@
+SELECT product_name, purchase_price
+  FROM Product
+ WHERE purchase_price NOT IN (500, 2800, 5000);
+/*
+ product_name | purchase_price
+--------------+----------------
+ 打孔器       |            320
+ 擦菜板       |            790
+(2 行记录)
+*/
+
+SELECT product_name, purchase_price
+  FROM Product
+ WHERE purchase_price NOT IN (500, 2800, 5000, NULL);
+
+/*
+ product_name | purchase_price
+--------------+----------------
+(0 行记录)
+*/
