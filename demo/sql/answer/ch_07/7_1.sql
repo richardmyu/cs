9f382b50fb18b26dac1cdc356350ac4fc4bc4d5f
yum <richardminyu@foxmail.com>
2021-12-31 01:30:53 +0800

    add sql 07

---
+++ demo/sql/answer/ch_07/7_1.sql
@@ -0,0 +1,13 @@
+SELECT *
+  FROM Product
+UNION
+SELECT *
+  FROM Product
+INTERSECT
+SELECT *
+  FROM Product
+ORDER BY product_id;
+
+/*
+Product
+*/
---
+++ demo/sql/answer/ch_07/7_2.sql
@@ -0,0 +1,7 @@
+SELECT CASE WHEN SP.shop_id IS NOT NULL THEN SP.shop_id ELSE '不确定' END,
+       CASE WHEN SP.shop_name IS NOT NULL THEN SP.shop_name ELSE '不确定' END,
+       P.product_id,
+       P.product_name,
+       P.sale_price
+  FROM ShopProduct AS SP RIGHT OUTER JOIN Product AS P
+    ON SP.product_id = P.product_id;
