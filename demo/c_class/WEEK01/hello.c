88305213ee3146d8344319b9662f1d24483fe98b
yum <richardminyu@foxmail.com>
2022-08-19 21:55:56 +0800

    refactor(/):refactor menu

--- demo/c_class/WEEK01/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("hello world!");
-	printf("季后赛的复活节");
-	return 0;
-}
--- demo/c_class/WEEK01/homework.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("hello world! 子曰");
-    return 0;
-}
