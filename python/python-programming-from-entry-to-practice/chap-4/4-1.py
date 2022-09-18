179794793d806721caa3ecf77d2780394de35c43
yum <richardminyu@foxmail.com>
2022-09-18 22:05:50 +0800

    feat(py): add python 4-1/2 files

---
+++ python/python-programming-from-entry-to-practice/chap-4/4-1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+foods = ['kuangfen', 'doupi', 'mantou']
+
+for food in foods:
+    print(f'I like {food}')
+print(f'I really like {foods[0]}')
---
+++ python/python-programming-from-entry-to-practice/chap-4/4-2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+animals = ['dog', 'cat', 'moncky']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet.')
+print('Any of these animal would make a great pet.')
