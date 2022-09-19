090a34f2f7cc87608d2f8e1b322fbd98235b65e5
yum <richardminyu@foxmail.com>
2022-09-19 23:22:04 +0800

    feat(py): add python chap-5 files

---
+++ python/python-programming-from-entry-to-practice/chap-5/5-1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+car = 'subaru'
+print('Is car == "subaru"? I predict True.')
+print(car == 'subaru')
+
+car = 'audi'
+print('Is car == "audi"? I predict True.')
+print(car == 'audi')
+
+age = 18
+print('Is car == 18? I predict True.')
+print(age == 18)
+
+age = 16
+print('Is car == 16? I predict True.')
+print(age == 16)
+
+age = 66
+print('Is car == 66? I predict True.')
+print(age == 66)
+
+car = 'subaru'
+print('Is car == "subarudd"? I predict True.')
+print(car == 'subarudd')
+
+car = 'audi'
+print('Is car == "bmw"? I predict True.')
+print(car == 'bmw')
+
+age = 18
+print('Is car == 8? I predict True.')
+print(age == 8)
+
+age = 16
+print('Is car == 66? I predict True.')
+print(age == 66)
+
+age = 66
+print('Is car == 6? I predict True.')
+print(age == 6)
