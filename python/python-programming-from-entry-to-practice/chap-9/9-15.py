56cd18f1fb96ade1423893229ba1dccd7e40d98c
yum <richardminyu@foxmail.com>
2022-09-29 22:59:19 +0800

    refactor(python): mod chap-9

--- python/python-programming-from-entry-to-practice/chap-9/9-15.py
+++ python/python-programming-from-entry-to-practice/chap-9/9-15.py
@@ -19,25 +19,25 @@ for n in range(1, 11):
 print('---' * 12)
 
 
-class Die_10(Die):
+class Die10(Die):
 
     def __init__(self, sides):
         super().__init__(sides)
 
 
-die_10 = Die_10(10)
+die_10 = Die10(10)
 for n in range(1, 11):
     die_10.roll_die()
 
 print('---' * 12)
 
 
-class Die_20(Die):
+class Die20(Die):
 
     def __init__(self, sides):
         super().__init__(sides)
 
 
-die_20 = Die_20(20)
+die_20 = Die20(20)
 for n in range(1, 11):
     die_20.roll_die()
