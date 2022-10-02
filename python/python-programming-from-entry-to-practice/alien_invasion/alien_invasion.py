3b332b9036989b01b9011132c22d81cb17ee67ee
yum <richardminyu@foxmail.com>
2022-10-02 11:20:57 +0800

    feat(py): add project- alien invasion part 1

--- python/python-programming-from-entry-to-practice/alien_invasion/alien_invasion.py
+++ python/python-programming-from-entry-to-practice/alien_invasion/alien_invasion.py
@@ -11,6 +11,9 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_function as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -21,17 +24,18 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    #
-    while True:
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    # 创建飞船
+    ship = Ship(ai_settings, screen)
 
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+    # 创建子弹编组
+    bullets = Group()
+
+    # 开始游戏主循环
+    while True:
+        gf.chunk_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
