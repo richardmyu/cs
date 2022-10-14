646e36389f461b4a94ef36a0d622a550358e424d
yum <richardminyu@foxmail.com>
2022-10-14 23:55:53 +0800

    refactor(language/c/head-first): update chap-1

---
+++ language/c/head-first-c/chap-1/card-21-new.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char card_name[3];
+  int count = 0;
+  do
+  {
+    puts("输入牌名：");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch (card_name[0])
+    {
+    case 'K':
+    case 'J':
+    case 'Q':
+      val = 10;
+      break;
+    case 'A':
+      val = 11;
+      break;
+    case 'X':
+      continue;
+    default:
+      val = atoi(card_name);
+    }
+    if ((val > 2) && (val < 7))
+    {
+
+      count++;
+    }
+    else if (val == 10)
+    {
+      count--;
+    }
+    printf("这张牌的点数是：%i\n", val);
+  } while (card_name[0] != 'X');
+
+  return 0;
+}
---
+++ language/c/head-first-c/chap-1/test2.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+void fn1()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5)
+  {
+    y = x - y;
+    printf("%i%i ", x, y);
+    x += 1;
+  }
+}
+
+void fn2()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5)
+  {
+    y = x + y;
+    printf("%i%i ", x, y);
+    x += 1;
+  }
+}
+
+void fn3()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5)
+  {
+    y = y + 2;
+    if (y > 4)
+    {
+      y -= 1;
+    }
+    printf("%i%i ", x, y);
+    x += 1;
+  }
+}
+
+void fn4()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5)
+  {
+    x = x + 1;
+    y = y + x;
+    printf("%i%i ", x, y);
+    x += 1;
+  }
+}
+
+void fn5()
+{
+  int x = 0;
+  int y = 0;
+  while (x < 5)
+  {
+    if (y < 5)
+    {
+      x = x + 1;
+      if (y < 3)
+      {
+        x -= 1;
+      }
+    }
+    y += 2;
+    printf("%i%i ", x, y);
+    x += 1;
+  }
+}
+
+int main()
+{
+  fn1();
+  printf("\n");
+  fn2();
+  printf("\n");
+  fn3();
+  printf("\n");
+  fn4();
+  printf("\n");
+  fn5();
+  return 0;
+}
