645d19c5cf5ab35402362481951992d6fd8836ca
yum <richardminyu@foxmail.com>
2022-10-17 23:59:33 +0800

    refactor(computer-system/sql): update

--- computer-system/SQL/chap-3/3_3.sql
+++ computer-system/SQL/chap-3/3_3.sql
@@ -1,3 +1,7 @@
-SELECT product_id, product_name, product_type, sale_price, purchase_price, regist_date
-  FROM Product
-  ORDER BY regist_date DESC, sale_price;
+SELECT
+  *
+FROM
+  Product
+ORDER BY
+  regist_date DESC,
+  sale_price;
