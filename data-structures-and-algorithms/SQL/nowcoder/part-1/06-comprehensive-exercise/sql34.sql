f03c74161684d4d77a408d1974c28218f8dd93be
yum <richardminyu@foxmail.com>
2022-11-07 00:26:08 +0800

    feat(data-al/sql/part-1): mod 34

--- data-structures-and-algorithms/SQL/nowcoder/part-1/06-comprehensive-exercise/sql34.sql
+++ data-structures-and-algorithms/SQL/nowcoder/part-1/06-comprehensive-exercise/sql34.sql
@@ -1,34 +1,56 @@
 -- 题目 统计复旦用户8月练题情况
 -- 关键字 综合练习
-TODO: 待解决
 SELECT
-  UP.device_id,
-  UP.university,
-  UP.question_cnt,
-  COUNT(*) AS right_question_cnt
+  UPF.device_id,
+  UPF.university,
+  CASE
+    b.question_cnt
+    WHEN b.question_cnt THEN 0
+    ELSE NULL
+  END AS b.question_cnt,
+  CASE
+    b.right_question_cnt
+    WHEN b.right_question_cnt THEN 0
+    ELSE NULL
+  END AS b.right_question_cnt
 FROM
-  (
-    SELECT
-      device_id,
-      university,
-      question_cnt
-    FROM
-      user_profile
-    WHERE
-      university = "复旦大学"
-  ) AS UP
+  user_profile AS UPF
   INNER JOIN (
     SELECT
       device_id,
-      result
+      university,
+      COUNT(result) AS question_cnt,
+      SUM(result_value) AS right_question_cnt
     FROM
-      question_practice_detail
-    WHERE
-      SUBSTRING(
-        date
+      (
+        SELECT
+          UP.device_id,
+          UP.university,
+          QPD.result,
+          CASE
+            QPD.result
+            WHEN 'right' THEN 1
+            WHEN 'wrong' THEN 0
+            ELSE NULL
+          END AS result_value
         FROM
-          6 FOR 2
-      ) = "08"
-  ) AS QD ON UP.device_id = QD.device_id
-GROUP BY
-  device_id;
+          question_practice_detail AS QPD
+          INNER JOIN user_profile UP ON QPD.device_id = UP.device_id
+        WHERE
+          UP.device_id IN (
+            SELECT
+              device_id
+            FROM
+              user_profile
+            WHERE
+              university = "复旦大学"
+          )
+          AND SUBSTRING(
+            date
+            FROM
+              6 FOR 2
+          ) = "08"
+      ) AS a
+  ) AS b ON UPF.device_id = b.device_id
+WHERE
+  university = "复旦大学";
