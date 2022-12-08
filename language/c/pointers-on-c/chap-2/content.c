aac27324877e403e2c0eaab7cd18b1c64b18a947
yum <richardminyu@foxmail.com>
2022-12-09 00:06:20 +0800

    feat(language/c/p-o-c): update chap-2

---
+++ language/c/pointers-on-c/chap-2/content.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Delete file (are you really sure??): ");
+
+  return 0;
+}
---
+++ language/c/pointers-on-c/chap-2/questions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+  return 0;
+}
+
+#if 0
+/* 2.7.1 */
+void squares(int limit)
+{
+  /* Comment out this entire funciton
+    int i; /* loop counter */
+  /*
+  ** Print table of squares
+  */
+  for (i = 0; i < limit; i += 1)
+  {
+    printf("%d %d0, i, i*i");
+  }
+  End of commented - out code * /
+}
+#endif
