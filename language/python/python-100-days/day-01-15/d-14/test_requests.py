b80b4d627a57fa04cacc56817ec6189bf4f6c2b5
yum <richardminyu@foxmail.com>
2023-02-23 19:24:41 +0800

    docs(language/python/python-100-days): update d-14

--- language/python/python-100-days/day-01-15/d-14/test_requests.py
+++ language/python/python-100-days/day-01-15/d-14/test_requests.py
@@ -4,7 +4,7 @@ from threading import Thread
 import requests
 
 
-# 继承Thread类创建自定义的线程类
+# 继承 Thread 类创建自定义的线程类
 class DownloadHanlder(Thread):
     def __init__(self, url):
         super().__init__()
@@ -28,11 +28,12 @@ def main():
     # 将服务器返回的JSON格式的数据解析为字典
     data_model = resp.json()
 
-    for mm_dict in data_model['newslist']:
-        url = mm_dict['picUrl']
+    if data_model['code'] == 200:
+        for mm_dict in data_model['newslist']:
+            url = mm_dict['picUrl']
 
-        # 通过多线程的方式实现图片下载
-        DownloadHanlder(url).start()
+            # 通过多线程的方式实现图片下载
+            DownloadHanlder(url).start()
 
 
 if __name__ == '__main__':
