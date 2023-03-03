19dd7fa86d3269a143a52ddc3e7227bb4960e534
yum <richardminyu@foxmail.com>
2023-03-03 19:52:52 +0800

    docs(language/python/python-100-days): update d-16

--- language/python/python-100-days/day-16-20/d-16/exercise_02.py
+++ language/python/python-100-days/day-16-20/d-16/exercise_02.py
@@ -6,8 +6,10 @@ def main():
     courses = ['语文', '数学', '英语']
     # 录入五个学生三门课程的成绩
     # 错误 - 参考http://pythontutor.com/visualize.html#mode=edit
+    # 这种写法，本质是 [None] * len(courses) 重复 len(names) 次
     # scores = [[None] * len(courses)] * len(names)
     scores = [[None] * len(courses) for _ in range(len(names))]
+
     for row, name in enumerate(names):
         for col, course in enumerate(courses):
             scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
--- language/python/python-100-days/day-16-20/d-16/exercise_04.py
+++ language/python/python-100-days/day-16-20/d-16/exercise_04.py
@@ -4,13 +4,27 @@
 import itertools
 
 # 产生ABCD的全排列
-print(itertools.permutations('ABCD'))
+print('--- ABCD 的全排列 ---')
+for i in itertools.permutations('ABCD'):
+    print(i)
+
+print()
 
 # 产生ABCDE的五选三组合
-itertools.combinations('ABCDE', 3)
+print('--- ABCDE 的五选三组合 ---')
+for x in itertools.combinations('ABCDE', 3):
+    print(x)
+
+print()
 
 # 产生ABCD和123的笛卡尔积
-itertools.product('ABCD', '123')
+print('--- ABCD 和 123 的笛卡尔积 ---')
+for y in itertools.product('ABCD', '123'):
+    print(y)
+
+print()
 
 # 产生ABC的无限循环序列
-itertools.cycle(('A', 'B', 'C'))
+print('--- ABCD 的全排列 ---')
+for n in itertools.cycle(('A', 'B', 'C')):
+    print(n)
