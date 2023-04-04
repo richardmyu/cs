fe4872dd98aa87d15daa5da252424877ddb820c9
yum <richardminyu@foxmail.com>
2023-04-04 20:39:50 +0800

    docs(source/_posts/): update

--- language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_01.py
+++ language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_01.py
@@ -23,33 +23,14 @@ from matplotlib import pyplot
 import numpy
 
 
-def seq_search(items: list, elem) -> int:
-    """顺序查找"""
-    for index, item in enumerate(items):
-        if elem == item:
-            return index
-    return -1
-
-
-def bin_search(items, elem):
-    """二分查找"""
-    start, end = 0, len(items) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if elem > items[mid]:
-            start = mid + 1
-        elif elem < items[mid]:
-            end = mid - 1
-        else:
-            return mid
-    return -1
-
-
 def main():
     """主函数（程序入口）"""
     num = 6
-    styles = ['r-.', 'g-*', 'b-o', 'y-x', 'c-^', 'm-+', 'k-d']
-    legends = ['对数', '线性', '线性对数', '平方', '立方', '几何级数', '阶乘']
+    styles_1 = ['r-.', 'g-*', 'b-o', 'y-x']
+    styles_2 = ['c-^', 'm-+', 'k-d']
+    legends_1 = ['对数', '线性', '线性对数', '平方']
+    legends_2 = ['立方', '几何级数', '阶乘']
+
     x_data = [x for x in range(1, num + 1)]
     y_data1 = [log2(y) for y in range(1, num + 1)]
     y_data2 = [y for y in range(1, num + 1)]
@@ -58,15 +39,27 @@ def main():
     y_data5 = [y**3 for y in range(1, num + 1)]
     y_data6 = [3**y for y in range(1, num + 1)]
     y_data7 = [factorial(y) for y in range(1, num + 1)]
-    y_datas = [y_data1, y_data2, y_data3, y_data4, y_data5, y_data6, y_data7]
+    y_datas_1 = [y_data1, y_data2, y_data3, y_data4]
+    y_datas_2 = [y_data5, y_data6, y_data7]
 
     pyplot.rcParams['font.sans-serif'] = ['SimHei']
 
-    for index, y_data in enumerate(y_datas):
-        pyplot.plot(x_data, y_data, styles[index])
-    pyplot.legend(legends)
+    # plot 1
+    pyplot.subplot(1, 2, 1)
+    for index, y_data in enumerate(y_datas_1):
+        pyplot.plot(x_data, y_data, styles_1[index])
+    pyplot.legend(legends_1)
+    pyplot.xticks(numpy.arange(1, 7, step=1))
+    pyplot.yticks(numpy.arange(0, 70, step=10))
+
+    # plot 2
+    pyplot.subplot(1, 2, 2)
+    for index, y_data in enumerate(y_datas_2):
+        pyplot.plot(x_data, y_data, styles_2[index])
+    pyplot.legend(legends_2)
     pyplot.xticks(numpy.arange(1, 7, step=1))
     pyplot.yticks(numpy.arange(0, 751, step=50))
+
     pyplot.show()
 
 
