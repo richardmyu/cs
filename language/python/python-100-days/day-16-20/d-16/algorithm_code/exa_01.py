ae9f9a9e7166af954511954dbcfedee77e56f6b9
yum <richardminyu@foxmail.com>
2023-04-09 20:34:22 +0800

    docs(language/python/python-100-days): update d-16

--- language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_01.py
+++ language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_01.py
@@ -25,20 +25,38 @@ import numpy
 
 def main():
     """主函数（程序入口）"""
-    num = 6
+    num_1 = 6
+    num_2 = 7
     styles_1 = ['r-.', 'g-*', 'b-o', 'y-x']
     styles_2 = ['c-^', 'm-+', 'k-d']
     legends_1 = ['对数', '线性', '线性对数', '平方']
     legends_2 = ['立方', '几何级数', '阶乘']
 
-    x_data = [x for x in range(1, num + 1)]
-    y_data1 = [log2(y) for y in range(1, num + 1)]
-    y_data2 = [y for y in range(1, num + 1)]
-    y_data3 = [y * log2(y) for y in range(1, num + 1)]
-    y_data4 = [y**2 for y in range(1, num + 1)]
-    y_data5 = [y**3 for y in range(1, num + 1)]
-    y_data6 = [3**y for y in range(1, num + 1)]
-    y_data7 = [factorial(y) for y in range(1, num + 1)]
+    # 横坐标
+    x_data_1 = [x for x in range(1, num_1 + 1)]
+    x_data_2 = [x for x in range(1, num_2 + 1)]
+
+    # 对数
+    y_data1 = [log2(y) for y in range(1, num_1 + 1)]
+
+    # 线性
+    y_data2 = [y for y in range(1, num_1 + 1)]
+
+    # 线性对数
+    y_data3 = [y * log2(y) for y in range(1, num_1 + 1)]
+
+    # 平方
+    y_data4 = [y**2 for y in range(1, num_1 + 1)]
+
+    # 立方
+    y_data5 = [y**3 for y in range(1, num_2 + 1)]
+
+    # 几何级数
+    y_data6 = [3**y for y in range(1, num_2 + 1)]
+
+    # 阶乘
+    y_data7 = [factorial(y) for y in range(1, num_2 + 1)]
+
     y_datas_1 = [y_data1, y_data2, y_data3, y_data4]
     y_datas_2 = [y_data5, y_data6, y_data7]
 
@@ -47,18 +65,18 @@ def main():
     # plot 1
     pyplot.subplot(1, 2, 1)
     for index, y_data in enumerate(y_datas_1):
-        pyplot.plot(x_data, y_data, styles_1[index])
+        pyplot.plot(x_data_1, y_data, styles_1[index])
     pyplot.legend(legends_1)
-    pyplot.xticks(numpy.arange(1, 7, step=1))
-    pyplot.yticks(numpy.arange(0, 70, step=10))
+    pyplot.xticks(numpy.arange(1, num_1+1, step=1))
+    pyplot.yticks(numpy.arange(0, (num_1+1)*10, step=10))
 
     # plot 2
     pyplot.subplot(1, 2, 2)
     for index, y_data in enumerate(y_datas_2):
-        pyplot.plot(x_data, y_data, styles_2[index])
+        pyplot.plot(x_data_2, y_data, styles_2[index])
     pyplot.legend(legends_2)
-    pyplot.xticks(numpy.arange(1, 7, step=1))
-    pyplot.yticks(numpy.arange(0, 751, step=50))
+    pyplot.xticks(numpy.arange(1, num_2+1, step=1))
+    pyplot.yticks(numpy.arange(0, (num_2+1)*700, step=700))
 
     pyplot.show()
 
