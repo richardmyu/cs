bf0fef2823f6d114490103f15b574995be0b898e
yum <richardminyu@foxmail.com>
2023-04-11 20:39:13 +0800

    docs(language/python/python-100-days): update

--- language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_02.py
+++ language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_02.py
@@ -144,10 +144,10 @@ def _partition(items, start, end, comp):
 def main():
     """主函数"""
     items = [35, 97, 12, 68, 55, 73, 81, 40]
-    # print(bubble_sort(items))
+    print(bubble_sort(items))
     # print(select_sort(items))
     # print(merge_sort(items))
-    print(quick_sort(items))
+    # print(quick_sort(items))
 
     items2 = [
         Person('Wang', 25),
@@ -155,10 +155,10 @@ def main():
         Person('Zhang', 50),
         Person('He', 20),
     ]
-    # print(bubble_sort(items2, comp=lambda p1, p2: p1.age > p2.age))
+    print(bubble_sort(items2, comp=lambda p1, p2: p1.age > p2.age))
     # print(select_sort(items2, comp=lambda p1, p2: p1.name < p2.name))
     # print(merge_sort(items2, comp=lambda p1, p2: p1.age <= p2.age))
-    print(quick_sort(items2, comp=lambda p1, p2: p1.age <= p2.age))
+    # print(quick_sort(items2, comp=lambda p1, p2: p1.age <= p2.age))
 
     items3 = ['apple', 'orange', 'watermelon', 'durian', 'pear']
     # print(bubble_sort(items3))
