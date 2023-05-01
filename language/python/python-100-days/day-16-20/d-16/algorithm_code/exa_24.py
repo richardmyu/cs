cce4c6486c6902ebff5dc56c88070c70d0cabeb6
yum <richardminyu@foxmail.com>
2023-05-01 23:09:08 +0800

    docs(language/python/python-100-days): update

--- language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_24.py
+++ language/python/python-100-days/day-16-20/d-16/algorithm_code/exa_24.py
@@ -1,18 +1,35 @@
-from unittest import TestCase
-from exa_02 import select_sort,merge
+"""
+aiohttp - 异步HTTP网络访问
+异步I/O（异步编程）- 只使用一个线程（单线程）来处理用户请求
+用户请求一旦被接纳，剩下的都是I/O操作，通过多路I/O复用也可以达到并发的效果
+这种做法与多线程相比可以让CPU利用率更高，因为没有线程切换的开销
+Redis/Node.js - 单线程 + 异步I/O
+Celery - 将要执行的耗时间的任务异步化处理
+异步I/O事件循环 - uvloop
+"""
 
-class TestExa02(TestCase):
-    def setUp(self):
-        self.data1 = [35, 97, 12, 68, 55, 73, 81, 40]
-        self.items1 = [12, 35, 68, 97]
-        self.items2 = [40, 55, 73, 81]
+import asyncio
+import re
+import aiohttp
 
-    def test_merge(self):
-        items=merge(self.items1,self.items2)
-        for i in range(len(items)-1):
-            self.assertLessEqual(items[i],items[i+1])
+async def fetch(session,url):
+    async with session.get(url,ssl=False) as resp:
+        return await resp.text()
 
-    def test_select_sort(self):
-        items=select_sort(self.data1)
-        for i in range(len(items)-1):
-            self.assertLessEqual(items[i],items[i+1])
+async def main():
+    pattern = re.compile(r'\<title\>(?P<title>.*)\<\/title\>')
+    urls = ('https://www.python.org/',
+            'https://git-scm.com/',
+            'https://www.jd.com/',
+            'https://www.taobao.com/',
+            'https://www.douban.com/')
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            html = await fetch(session, url)
+            print(pattern.search(html).group('title'))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
