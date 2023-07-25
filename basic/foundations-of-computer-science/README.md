563986840d3a100cfca3d9fc6e6589cb52cadd2c
yum <richardminyu@foxmail.com>
2023-07-25 12:36:02 +0800

    feat(language/c/t-c-p-l): update

--- basic/foundations-of-computer-science/README.md
+++ basic/foundations-of-computer-science/README.md
@@ -2,4 +2,3 @@
 
 [美]Alfred V.Aho Jeffrey D.Ullman 著
 人民邮电出版社
-
--- language/c/the-c-programming-language/chap-01/count_word.c
+++ language/c/the-c-programming-language/chap-01/count_word.c
@@ -1,13 +1,41 @@
-#include <stdint.h>
+#include <stdio.h>
 
 #define IN 1  /*  在单词内 */
 #define OUT 0 /*  在单词外 */
 
 /* 统计输入的行数、单词数与字符数 */
+/* TODO: bug
+
+case 01
+---------------
+hello
+
+
+world
+
+3 2 13
+---------------
+
+case 02
+---------------
+hello
+
+
+world
+2 1 7
+---------------
+
+最后一行的单词句子，如果没有确定(Enter)，则不参与计数
+
+*/
 int main()
 {
+  /*
+    nl 行数
+    nw 单词数
+    nc 字符数
+  */
   int c, nl, nw, nc, state;
-
   state = OUT;
   nl = nw = nc = 0;
 
@@ -20,14 +48,13 @@ int main()
 
     if (c == ' ' || c == '\n' || c == '\t')
       state = OUT;
-    else
+
+    else if (state == OUT)
     {
-      if (state == OUT)
-      {
-        state = IN;
-        ++nw;
-      }
-      printf("%d %d %d\n", nl, nw, nc);
+      state = IN;
+      ++nw;
     }
   }
+
+  printf("\n%d %d %d\n", nl, nw, nc);
 }
