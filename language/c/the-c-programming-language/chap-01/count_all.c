46ba841ce34abf3418796e247da8226ac746f3d8
yum <richardminyu@foxmail.com>
2023-07-27 22:18:50 +0800

    feat(language/c/t-c-p-l): update

---
+++ language/c/the-c-programming-language/chap-01/count_all.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/* 统计各个数字、空白字符及其他字符分别出现的次数 */
+
+int main()
+{
+  int c, i, nwhite, nother;
+
+  /* 数组
+  用于把 ndigit 说明为由 10 个整数组成的数组
+  在 C 语言中，数组下标总是从 0 开始
+  */
+  int ndigit[10];
+
+  nwhite = nother = 0;
+
+  for (i = 0; i < 10; i++)
+  {
+    ndigit[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF)
+  {
+    if (c >= '0' && c <= '9')
+    {
+      ++ndigit[c - '0'];
+    }
+    else if (c == ' ' || c == '\n' || c == '\t')
+    {
+      ++nwhite;
+    }
+    else
+    {
+      ++nother;
+    }
+  }
+
+  printf("digits = ");
+
+  for (i = 0; i < 10; i++)
+  {
+    printf("  %d", ndigit[i]);
+    printf(", white space = %d, other = %d", nwhite, nother);
+  }
+}
