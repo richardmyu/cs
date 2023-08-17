f61f756dfa55763ea0ebd4260889b9279a2f1a14
yum <richardminyu@foxmail.com>
2023-08-17 12:24:44 +0800

    feat(language/c/c-p-p): update chap-3

---
+++ language/c/c-primer-plus/chap-02/code/3-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+  float weight; /* 体重 */
+  float value;  /* 相同重量的白金价值 */
+
+  printf("Are you worth your weight in platinum?\n");
+  printf("Let's check it out.\n");
+  printf("Please enter your weight in pounds: ");
+
+  // 获取输入
+  scanf("%f", &weight);
+  /*
+  假设白金的价格是每盎司 $1700
+
+  */
+  value = 1700.0 * weight * 14.5833;
+  printf("Are you worth your weight $%.2f.\n", value);
+  printf("You are easily worth that! If platinum prices drop,\n");
+  printf("eat more to maintain your value.\n");
+
+  return 0;
+}
