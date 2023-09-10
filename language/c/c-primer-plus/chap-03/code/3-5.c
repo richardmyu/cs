a47efbdd4c8e74a7b892a50e5e2013f27bc428b0
yum <richardminyu@foxmail.com>
2023-09-10 11:45:49 +0800

    feat(language/c/c-p-p): update chap-3

--- language/c/c-primer-plus/chap-03/code/3-5.c
+++ language/c/c-primer-plus/chap-03/code/3-5.c
@@ -6,7 +6,7 @@ int main(void)
 
   printf("Please enter a character.\n");
   scanf("%c", &ch);
-  printf("The code for %c is $d.\n", ch, ch);
+  printf("The code for %c is %d.\n", ch, ch);
 
   return 0;
 }
