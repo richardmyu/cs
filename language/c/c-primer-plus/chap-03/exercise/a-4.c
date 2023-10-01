668b467d053a7855d098d68ff7b52ea8ba2cd09c
yum <richardminyu@foxmail.com>
2023-10-01 22:29:33 +0800

    feat(language/c/c-p-p): update

---
+++ language/c/c-primer-plus/chap-03/exercise/a-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // a.'\b'
+  char _a = '\b';
+
+  // b.1066
+  int _b = 1066;
+
+  // c.99.44
+  float _c = 99.44;
+
+  // d.0XAA
+  int _d = 0XAA;
+  long _d1 = 0XAA;
+
+  // e.2.0e30
+  float _e = 2.0e30;
+
+  return 0;
+}
---
+++ language/c/c-primer-plus/chap-03/exercise/a-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+main
+(
+  float g; h;
+  float tax, rate;
+
+  g = e21;
+  tax = rate * g;
+)
+*/
+
+int main(void)
+{
+  float g, h;
+  float tax, rate;
+
+  g = 2e21;
+  tax = rate * g;
+
+  return 0;
+}
